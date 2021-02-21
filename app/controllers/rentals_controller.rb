c67d15aef22fc6a0334f3acc4c90d5fa17c5d40c
shota <#shota.gad1@gmail.com>
2021-02-21 20:27:36 +0900

    登録フォーム編集

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -9,8 +9,32 @@ class RentalsController < ApplicationController
   end
 
   def create
+    @rental = Rental.new(rentals_params)
+    if @rental.save
+      redirect_to rentals_path, notice: "物件を登録しました！"
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def rentals_params
+    params.require(:rental).permit(
+      :name,
+      :rent,
+      :address,
+      :age,
+      :note,
+      stations_attributes: [
+        :route,
+        :name,
+        :distance,
+        :rentals_id,
+        :id,
+        :_destroy,
+      ],
+    )
   end
 end
